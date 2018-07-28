9db0eaed6ab010747931cf649772ba5de50caece
Jun ho Lee <june20516@gmail.com>
2018-07-28 19:08:24 +0900

    dom과 HTML 연습

--- dom_test.js
+++ dom_test.js
@@ -1,1 +1,11 @@
-console.log('hello JS');
+var readInput = function () {
+    var ipt = document.getElementById('input1');
+    console.log(ipt.value);
+};
+
+
+var btn2 = document.createElement('button');
+btn2.onclick = readInput;
+btn2.innerHTML = 'button2';
+var test = document.getElementById('test');
+test.appendChild(btn2);
