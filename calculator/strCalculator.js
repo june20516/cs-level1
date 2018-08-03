421909287ce1b68b330efb56d07adf6c6b256909
Jun ho Lee <june20516@gmail.com>
2018-08-04 02:39:31 +0900

    GUI 계산기 업데이트
    일반 계산기와 같도록 수정
    - 숫자 입력 후 연산자 입력 시 기존 계산 결과 뜨도록 함
    - 연산자를 계속 누를 시 반복된 계산을 수행하도록 함

--- calculator/strCalculator.js
+++ calculator/strCalculator.js
@@ -3,44 +3,65 @@ var Box = document.getElementById('outbox');
             //숫자 입력
             ipt = {};
             ipt.ar = [];
-            ipt.N = 0;
+            ipt.n1 = 0; //먼저 입력된 값. 초기에는 0
+            ipt.n2 = 0; //나중 입력된 값. 초기에는 첫번째 누른 숫자.
             
-            ipt.Num = function(a) {
+            ipt.Num = function(a) { //나중 입력된 값을 배열로 입력받고 숫자로 바꿈.
                 this.ar.push(a);
-                opt.Num();
+                this.n2 = Number(this.ar.join(''));
+                opt.Num(this.n2);
+                console.log("num2 : ", this.n2," , num1 : " ,this.n1);
             }
+
+            ipt.Bs = function() {  //입력받은 숫자 지움
+                this.ar.pop();
+                this.n2 = Number(this.ar.join(''));
+                opt.Num(this.n2);
+            }
+
             //숫자 출력
             opt = {};
-            
-            opt.Num = function() {
-                ipt.N = ipt.ar.join('')*1;
-                Box.innerHTML = ipt.N;
-            };
+            opt.Num = function(a) {  //숫자를 박스에 출력
+                Box.innerHTML = a;   
+            }
+
             //숫자 계산
             cal = {};
-            cal.opr = ("");
-            cal.keep = 0;
-            cal.Next = function() {
+            cal.opr1 = (''); //첫번째 연산자.
+            cal.opr2 = (''); //이후 계산 연산자.
+            cal.opin = true;
+            cal.Next = function() {  //다음 값을 입력 받기 위한 배열 초기화.
                 ipt.ar = [];
-                this.keep = ipt.N;
+                this.opr1 = this.opr2;
             }
     
-            cal.opt = function(a) {
+            cal.opt = function(a) {  //연산자를 입력하였을 때 작동하는 함수.   
+                // this.init(); //초기화 인자 조절. =를 누른 이후에는 연산자 입력시에 기존 계산 안되도록.
+                this.calcul(this.opr1); //1. 기존 계산을 완료,
+                this.opr2 = a; //2. 연산자를 지금 눌러진 연산자로 변경.
+                opt.Num(ipt.n1);  // 3. 박스에 결과 출력 
+                console.log("opt : ",a,' , ', ipt.n1);
                 this.Next ();
-                this.opr = a;
             }
     
-            cal.eqr = function (opr) {
+            cal.calcul = function (opr) {
                 switch (opr) {
-                    case "+" : ans = this.keep + ipt.N
+                    case "+" : ipt.n1 += ipt.n2
                         break;
-                    case "-" : ans = this.keep - ipt.N
+                    case "-" : ipt.n1 -= ipt.n2
                         break;
-                    case "X" : ans = this.keep * ipt.N
+                    case "X" : ipt.n1 *= ipt.n2
                         break;
-                    case "÷" : ans = this.keep / ipt.N
+                    case "÷" : ipt.n1 /= ipt.n2
                         break;
+                    case "=" : 
+                        break;
+                    default : ipt.n1 = ipt.n2
                 }
-                Box.innerHTML = ans;
-                ipt.N = ans;
+            }
+
+            cal.equal = function () {
+                this.calcul(this.opr1);
+                opt.Num(ipt.n1);
+                this.opr1 = '=';
             }
